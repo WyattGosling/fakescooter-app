9cda41f49565f839f8f5d4acc5ed3ca803d23e78
Wyatt Gosling <wyatt.gosling@icloud.com>
2025-01-19 20:53:53 -0800

    Actually add recent changes to git
    I misunderstood what was happening in XCode's git UI. It looked
    like it was adding ALL changed files to the commit, but only the
    first change was.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 Fake E-Scoot Sharing Service: iOS App
 
 Just a dummpy app used to practice. This repository contains the iOS app that a user would use.
+
+#Attributions
+[kick scooter icons">Kick scooter icons created by Freepik - Flaticon]s(https://www.flaticon.com/free-icons/kick-scooter) 
--- fakescooter-app/ContentView.swift
+++ fakescooter-app/ContentView.swift
@@ -5,18 +5,37 @@
 //  Created by Wyatt Gosling on 2025-01-19.
 //
 
+import MapKit
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Map() {
+                ForEach(scooters) { scooter in
+                    Annotation("\(scooter.batteryLevel)%", coordinate: scooter.location) {
+                        Image("kick-scooter")
+                            .resizable()
+                            .frame(width: 30, height: 30)
+                    }
+                }
+            }
         }
         .padding()
     }
+
+    let userLocation = MapCameraPosition.region(
+        MKCoordinateRegion(
+            center: .init(latitude: 49.26359, longitude: 123.13854),
+            span: .init(latitudeDelta: 0.01, longitudeDelta: 0.01)
+        )
+    )
+    let scooters: [Scooter] = [
+        .init(id: "abc123", batteryLevel: 99, location: .init(latitude: 49.26227, longitude: -123.14242)),
+        .init(id: "def456", batteryLevel: 88, location: .init(latitude: 49.26636, longitude: -123.14226)),
+        .init(id: "ghi789", batteryLevel: 77, location: .init(latitude: 49.26532, longitude: -123.13659)),
+        .init(id: "jkl012", batteryLevel: 9, location: .init(latitude: 49.26443, longitude: -123.13469))
+    ]
 }
 
 #Preview {
