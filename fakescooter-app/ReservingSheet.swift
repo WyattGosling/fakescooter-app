ac6a30a6bf7fa1f6d89c2cd521008eac5b8a061e
Wyatt Gosling <wyatt.gosling@icloud.com>
2025-03-02 16:51:03 -0800

    Error handling in ReservingSheet.reserve
    Previously if the user already had a reservation, this would just fail. Now it will try to lookup the existing reservation and use that instead.

--- fakescooter-app/ReservingSheet.swift
+++ fakescooter-app/ReservingSheet.swift
@@ -77,10 +77,26 @@ struct ReservingSheet: View {
         request.httpBody = try! JSONEncoder().encode(["reserved": true])
         request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
         
-        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+        let task = URLSession.shared.dataTask(with: request) { data, resp, error in
+            guard error == nil else {
+                print("Error reserving scooter: \(String(describing: error))")
+                return
+            }
+            guard let resp = resp as? HTTPURLResponse else {
+                print("Error with the url response")
+                return
+            }
             guard let data else {
                 print("Error reserving scooter: \(String(describing: error))")
                 return
+            }
+            
+            if resp.statusCode != 200 {
+                let errorString = String(data: data, encoding: .utf8) ?? "no data"
+                if errorString == "user \(currentUser.name) already has a scooter reserved" {
+                    lookupExistingReservation();
+                    dismiss()
+                }
             }
 
             let decoder = JSONDecoder()
@@ -89,6 +105,52 @@ struct ReservingSheet: View {
             dismiss()
         }
         task.resume()
+    }
+
+    /*
+     * A request responded stating the current user already has a reservation.
+     * Lookup that reservation and set states accordingly.
+     */
+    func lookupExistingReservation() {
+        // Oh hey, it is this code again
+        let base64LoginString = String(format: "%@:%@", currentUser.name, "pas")
+            .data(using: .utf8)!
+            .base64EncodedString()
+        
+        let url = Config.baseURL.appending(
+            components: "scooter",
+            directoryHint: .notDirectory
+        ).appending(queryItems: [
+            .init(name: "user", value: currentUser.id)
+        ])
+
+        var request = URLRequest(url: url)
+        request.httpMethod = "GET"
+        request.setValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+
+        let task = URLSession.shared.dataTask(with: request) { data, resp, error in
+            guard error == nil else {
+                print("Error reserving scooter: \(String(describing: error))")
+                return
+            }
+            guard let resp = resp as? HTTPURLResponse else {
+                print("Error with the url response")
+                return
+            }
+            guard let data else {
+                print("Error reserving scooter: \(String(describing: error))")
+                return
+            }
+            guard resp.statusCode == 200 else {
+                let responseString = String(data: data, encoding: .utf8) ?? "No data returned"
+                print("Error reserving scooter: \(resp.statusCode), \(responseString)")
+                return
+            }
+            
+            let decoder = JSONDecoder()
+            let scooter = try! decoder.decode(Scooter.self, from: data)
+            reservation = scooter
+        }
     }
     
     @Binding var alreadyReserved: Bool
